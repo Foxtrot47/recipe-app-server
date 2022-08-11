518da70576549fdc5cd7f8c790d1f26a8e1672f6
Foxtrot47 <jjneutron@outlook.com>
2022-08-11 22:16:19 +0530

    Implement basic search api

--- db.js
+++ db.js
@@ -8,7 +8,27 @@ mongoose.connect(process.env.DATABSE_URL).catch((err) => {
 });
 
 const recipeSchema = new mongoose.Schema(
-  { _id: "number", name: "string", slug: "string" },
+  {
+    _id: "number",
+    name: "string",
+    author: "string",
+    slug: "string",
+    description: "string",
+    date: "date",
+    rating: "object",
+    keywords: "array",
+    nutritionalInfo: "array",
+    category: "array",
+    diet: "array",
+    cuisine: "array",
+    ingredients: "array",
+    instructions: "array",
+    yield: "number",
+    image: "object",
+    skillLevel: "string",
+    time: "object",
+    similarRecipes: "array",
+  },
   { collection: "recipedata" }
 );
 const Recipe = mongoose.model("Recipe", recipeSchema);
--- routes.js
+++ routes.js
@@ -77,6 +77,78 @@ const routes = async (server) => {
 
   server.route({
     method: "GET",
+    url: "/search",
+    schema: {
+      querystring: {
+        type: "object",
+        properties: {
+          q: { type: "string", minLength: 1 },
+        },
+      },
+    },
+    handler: async (request) => {
+      const query = {};
+      let offset = 0,
+        sort = [];
+      query.name = new RegExp(request.query.q, "i");
+      if (request.query.cuisine) query.cuisine = request.query.cuisine;
+      if (request.query.keywords) query.keywords = request.query.keywords;
+      if (request.query.category) query.category = request.query.category;
+      if (request.query.skillLevel) query.skillLevel = request.query.skillLevel;
+      if (request.query.yield) {
+        if (request.query.yield === "9+") query.yield = { $gt: 8 };
+        else query.yield = request.query.yield;
+      }
+      if (request.query.totalTime) {
+        if (request.query.totalTime === "lte10")
+          query["time.totalTime"] = { $lte: 10 };
+        else if (request.query.totalTime === "lte30")
+          query["time.totalTime"] = { $lte: 30 };
+        else if (request.query.totalTime === "lte60")
+          query["time.totalTime"] = { $lte: 60 };
+        else if (request.query.totalTime === "gt60")
+          query["time.totalTime"] = { $gt: 60 }; 
+      }
+
+      if (request.query.kcal) {
+        query["nutritionalInfo"] = { $elemMatch: { label: "kcal" } };
+        if (request.query.kcal === "lte250")
+          query["nutritionalInfo"].$elemMatch.value = { $lte: 250 };
+        else if (request.query.kcal === "lte500")
+          query["nutritionalInfo"].$elemMatch.value = { $lte: 500 };
+        else if (request.query.kcal === "lte750")
+          query["nutritionalInfo"].$elemMatch.value = { $lte: 750 };
+        else if (request.query.kcal === "lte1000")
+          query["nutritionalInfo"].$elemMatch.value = { $lte: 1000 };
+        else if (request.query.kcal === "lte1500")
+          query["nutritionalInfo"].$elemMatch.value = { $lte: 1500 };
+        else if (request.query.kcal === "gt1500")
+          query["nutritionalInfo"].$elemMatch.value = { $gt: 1500 };
+      }
+
+      if (request.query.rating) {
+        if (request.query.rating === "gte1") query["rating.avg"] = { $gte: 1 };
+        else if (request.query.rating === "gte2")
+          query["rating.avg"] = { $gte: 2 };
+        else if (request.query.rating === "gte3")
+          query["rating.avg"] = { $gte: 3 };
+        else if (request.query.rating === "gte4")
+          query["rating.avg"] = { $gte: 4 };
+        else if (request.query.rating === "eq5") query["rating.avg"] = 5;
+      }
+      if (request.query.offset) offset = request.query.offset;
+      if (request.query.sortBy && request.query.sortOrder)
+        sort = [[request.query.sortBy, parseInt(request.query.sortOrder)]];
+      console.log(query.nutritionalInfo);
+      return await Recipe.find(query, "_id name slug image rating date", {
+        limit: 20,
+        offset,
+      }).sort(sort);
+    },
+  });
+
+  server.route({
+    method: "GET",
     url: "/autocomplete",
     schema: {
       querystring: {
